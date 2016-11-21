a06b5c772bf05d89141aea974d43397559ed9970
paradise8097 <paradise8097@users.noreply.github.com>
2016-11-21 06:22:44 -0800

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>The Shape of Things to Come</title>
+  </head>
+  <body>
+    <p> <?php
+    class Shape {
+    public $hasSides = true;
+    }
+
+    class Square extends Shape {
+
+    }
+
+    $square = new Square();
+    // Add your code below!\
+
+
+
+
+
+
+
+
+    if (property_exists($square, "hasSides")) {
+      echo "I have sides!";
+    }
+  ?>
+</p>
+  </body>
+</html>
